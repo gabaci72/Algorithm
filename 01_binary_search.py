bb646baceb78e85af85024f4db110c04138b442e
gabaci72 <PC@DESKTOP-VATGGKT>
2024-08-18 11:42:09 +0200

    syntax corrected

--- 01_binary_search.py
+++ 01_binary_search.py
@@ -1,4 +1,4 @@
-cd def binary_search(list, item):
+def binary_search(list, item):
   # low and high keep track of which part of the list you'll search in.
   low = 0
   high = len(list) - 1
@@ -22,7 +22,6 @@ cd def binary_search(list, item):
   return None
 
 my_list = [1, 3, 5, 7, 9]
-print binary_search(my_list, 3) # => 1
-
+print(binary_search(my_list, 3))  # => 1
 # 'None' means nil in Python. We use to indicate that the item wasn't found.
-print binary_search(my_list, -1) # => None
+print(binary_search(my_list, -1))  # => None
